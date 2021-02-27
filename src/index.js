00513095d8572afa22ab730d46e467e81310fc99
Dmitry Zakharov <dzakh.dev@gmail.com>
2021-02-27 23:32:34 +0300

    Fix actual rule name

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { consistentImportRule } from '@/rules/consistentImport';
 
 export const rules = {
-  'consistent-import-name': consistentImportRule,
+  'consistent-import': consistentImportRule,
 };
